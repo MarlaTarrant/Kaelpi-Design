bcf39a827bfc359d0aa21718b73b74e21dd93727
Marla Tarrant <tarrantmarlajean@gmail.com>
2019-03-27 16:07:42 +0200

    contact form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,64 +1,69 @@
-(function($){
+$(function() {
 
-	$(document).ready(function() {
+    $("input,textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var message = $("textarea#message").val();
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+            $.ajax({
+                url: "assets/contact.php",
+                type: "POST",
+                data: {
+                    name: name,
+                    email: email,
+                    message: message,
+                    submit: 1
+                },
+                cache: false,
+                success: function() {
+                    // Success message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Thank you, your message has been sent. We will get back to you shortly.</strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
 
-		/* ---------------------------------------------- /*
-		 * Contact form ajax
-		/* ---------------------------------------------- */
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that our mail server is not responding. Please email kaelpidesign@gmail.com if the problem persists.");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+            })
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
 
-		$('#contact-form').find('input,textarea').jqBootstrapValidation({
-			preventSubmit: true,
-			submitError: function($form, event, errors) {
-				// additional error messages or events
-			},
-			submitSuccess: function($form, event) {
-				event.preventDefault();
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
 
-				var name            = $("input#name").val();
-				var email           = $("input#email").val();
-				var message         = $("textarea#message").val();
-				var submit          = $('#contact-form submit');
-				var ajaxResponse    = $('#contact-response');
 
-				$.ajax({
-					url: 'assets/contact.php',
-					type: 'POST',
-					dataType: 'json',
-					data: {
-						name: name,
-						email: email,
-						message: message,
-						submit: 1
-					},
-					cache: false,
-					success: function() {
-						// Success message
-						$('#success').html("<div class='alert alert-success'>");
-						$('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-							.append("</button>");
-						$('#success > .alert-success')
-							.append("<strong>Thank you, your message has been sent. We will get back to you shortly.</strong>");
-						$('#success > .alert-success')
-							.append('</div>');
-	
-						//clear all fields
-						$('#contact-form').trigger("reset");
-					},
-					error: function() {
-						// Fail message
-						$('#success').html("<div class='alert alert-danger'>");
-						$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-							.append("</button>");
-						$('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that our mail server is not responding. Please email kaelpidesign@gmail.com if the problem persists.");
-						$('#success > .alert-danger').append('</div>');
-						//clear all fields
-						$('#contact-form').trigger("reset");
-					},
-
-				});
-			}
-		});
-
-	});
-
-})(jQuery);
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
+});
