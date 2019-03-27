bf59948e09df650c51ed14b367af09b4118ee879
Marla Tarrant <tarrantmarlajean@gmail.com>
2019-03-27 15:44:12 +0200

    contact form

--- assets/contact.php
+++ assets/contact.php
@@ -1,33 +1,20 @@
 <?php
 
-    $result="";
-
     if (isset($_POST['submit'])) {
-        require '/assets/phpmailer/PHPMailerAutoload.php';
-        $mail = new PHPMailer;
 
-        $mail->isSMTP();
-        $mail->Host='smtp.margineco.com';
-        $mail->Port='587';
-        $mail->SMTPAuth=on;
-        $mail->SMTPSecure='tls';
-        $mail->Username='kaelpidesign@gmail.com';
-        $mail->Password='K@3lp1410961107';
+        var_dump($_POST);
 
-        $mail->setFrom($_POST['email'],$_POST['name']);
-        $mail->addAddress('mail.margineco.com');
-        $mail->addReplyTo($_POST['email'],$_POST['name']);
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
 
-        $mail->isHTML(true);
-        $mail->Subject='Form Submission: '.$_POST['subject'];
-        $mail->Body='<h1 align=center>Name :'.$_POST['name'].'<br>Email: '.$_POST['email'].'<br>Message: '.$_POST['message'].'</h1>';
+        $mailTo = "marla@margineco.com";
+        $subject = "New message from ".$name;
+        $headers = "From: ".$email;
+        $txt = "You have received an email from ".$name.".\n\n".$message;
 
-        if(!$mail->send()){
-            $result="Something went wrong. Please try again.";
-        }
-        else {
-            $result="Thanks ".$_POST['name']." for contacting us.";
-        }
+        mail($mailTo, $subject, $txt, $headers);
+        header("Location: index.html?mailsend");
     }
 ?>
 
--- assets/js/contact.js
+++ assets/js/contact.js
@@ -18,7 +18,6 @@
 				var email           = $("input#email").val();
 				var message         = $("textarea#message").val();
 				var submit          = $('#contact-form submit');
-				var ajaxResponse    = $('#contact-response');
 
 				$.ajax({
 					url: 'assets/contact.php',
@@ -31,18 +30,30 @@
 						submit: 1
 					},
 					cache: false,
-					beforeSend: function(result) {
-						submit.empty();
-						submit.append('<i class="fa fa-cog fa-spin"></i> Wait...');
+					success: function() {
+						// Success message
+						$('#success').html("<div class='alert alert-success'>");
+						$('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+							.append("</button>");
+						$('#success > .alert-success')
+							.append("<strong>Thank you, your message has been sent. We will get back to you shortly.</strong>");
+						$('#success > .alert-success')
+							.append('</div>');
+	
+						//clear all fields
+						$('#contact-form').trigger("reset");
 					},
-					success: function(result) {
-						if(result.sendstatus == 1) {
-							ajaxResponse.html(result.message);
-							$form.fadeOut(500);
-						} else {
-							ajaxResponse.html(result.message);
-						}
-					}
+					error: function() {
+						// Fail message
+						$('#success').html("<div class='alert alert-danger'>");
+						$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+							.append("</button>");
+						$('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that our mail server is not responding. Please email support@margineco.com if the problem persists.");
+						$('#success > .alert-danger').append('</div>');
+						//clear all fields
+						$('#contact-form').trigger("reset");
+					},
+
 				});
 			}
 		});
