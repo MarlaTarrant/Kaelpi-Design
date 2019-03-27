b7c3b4e43454bdad5c1f63608b5c76ececdac913
Marla Tarrant <tarrantmarlajean@gmail.com>
2019-03-27 16:17:09 +0200

    contact form update

--- assets/contact.php
+++ assets/contact.php
@@ -4,18 +4,11 @@
 
         var_dump($_POST);
 
-        $mail->Host='mail.margineco.com';
-        $mail->Port='587';
-        $mail->SMTPAuth=true;
-        $mail->SMTPSecure='tls';
-        $mail->Username='marla@margineco.com';
-        $mail->Password='iYEYl4B#*0*';
-
         $name = $_POST['name'];
         $email = $_POST['email'];
         $message = $_POST['message'];
 
-        $mailTo = "marla@margineco.com";
+        $mailTo = "kaelpidesign@gmail.com";
         $subject = "New message from ".$name;
         $headers = "From: ".$email;
         $txt = "You have received an email from ".$name.".\n\n".$message;
--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,69 +1,78 @@
-$(function() {
+(function($){
 
-    $("input,textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
-        },
-        submitSuccess: function($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
-            // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
+	$(document).ready(function() {
+
+		/* ---------------------------------------------- /*
+		 * Contact form ajax
+		/* ---------------------------------------------- */
+
+		$('#contact-form').find('input,textarea').jqBootstrapValidation({
+			preventSubmit: true,
+			submitError: function($form, event, errors) {
+				// additional error messages or events
+			},
+			submitSuccess: function($form, event) {
+				event.preventDefault();
+
+				var name            = $("input#name").val();
+				var email           = $("input#email").val();
+				var message         = $("textarea#message").val();
+				var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            $.ajax({
-                url: "assets/contact.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    email: email,
-                    message: message,
-                    submit: 1
-                },
-                cache: false,
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Thank you, your message has been sent. We will get back to you shortly.</strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that our mail server is not responding. Please email kaelpidesign@gmail.com if the problem persists.");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
-        },
-        filter: function() {
-            return $(this).is(":visible");
-        },
-    });
+				$.ajax({
+					url: 'assets/contact.php',
+					type: 'POST',
+					dataType: 'json',
+					data: {
+						name: name,
+						email: email,
+						message: message,
+						submit: 1
+					},
+					cache: false,
+					success: function() {
+						// Success message
+						$('#success').html("<div class='alert alert-success'>");
+						$('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+							.append("</button>");
+						$('#success > .alert-success')
+							.append("<strong>Thank you, your message has been sent. We will get back to you shortly.</strong>");
+						$('#success > .alert-success')
+							.append('</div>');
+	
+						//clear all fields
+						$('#contact-form').trigger("reset");
+					},
+					error: function() {
+						// Fail message
+						$('#success').html("<div class='alert alert-danger'>");
+						$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+							.append("</button>");
+						$('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that our mail server is not responding. Please email kaelpidesign@gmail.com if the problem persists.");
+						$('#success > .alert-danger').append('</div>');
+						//clear all fields
+						$('#contact-form').trigger("reset");
+					},
+					beforeSend: function(result) {
+						submit.empty();
+						submit.append('<i class="fa fa-cog fa-spin"></i> Wait...');
+					},
+					success: function(result) {
+						if(result.sendstatus == 1) {
+							ajaxResponse.html(result.message);
+							$form.fadeOut(500);
+						} else {
+							ajaxResponse.html(result.message);
+						}
+					}
+				});
+			}
+		});
 
-    $("a[data-toggle=\"tab\"]").click(function(e) {
-        e.preventDefault();
-        $(this).tab("show");
-    });
-});
+	});
 
-
-/*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
-    $('#success').html('');
-});
+})(jQuery);
