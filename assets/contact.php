8549bfce8b4c1b5727053b34ce4f25a8b83bd7b2
Marla Tarrant <tarrantmarlajean@gmail.com>
2019-03-29 15:09:37 +0200

    email address change and file upload

--- assets/contact.php
+++ assets/contact.php
@@ -21,7 +21,7 @@ try {
     $mail->isSMTP();                                            // Set mailer to use SMTP
     $mail->Host       = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'kaelpidesign@gmail.com';               // SMTP username
+    $mail->Username   = 'tarrantmarlajean@gmail.com';               // SMTP username
     $mail->Password   = 'K@elp1410961107';                      // SMTP password
     $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
     $mail->Port       = 587;                                    // TCP port to connect to
