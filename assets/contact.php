e90d68d708408e839484f6bfbd199fae0f155390
Marla Tarrant <tarrantmarlajean@gmail.com>
2019-03-29 15:06:20 +0200

    contact form edit

--- assets/contact.php
+++ assets/contact.php
@@ -1,20 +1,46 @@
 <?php
 
-    if (isset($_POST['submit'])) {
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-        var_dump($_POST);
+// Load Composer's autoloader
+require 'vendor/autoload.php';
 
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $message = $_POST['message'];
+$mail = new PHPMailer(true);
 
-        $mailTo = "kaelpidesign@gmail.com";
-        $subject = "New message from ".$name;
-        $headers = "From: ".$email;
-        $txt = "You have received an email from ".$name.".\n\n".$message;
+try {
+    if(isset($_POST['send'])){
 
-        mail($mailTo, $subject, $txt, $headers);
-        header("Location: index.html?mailsend");
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $subject = $_POST['subject']
+        
+    //Server settings
+    $mail->SMTPDebug = 2;                                       // Enable verbose debug output
+    $mail->isSMTP();                                            // Set mailer to use SMTP
+    $mail->Host       = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'kaelpidesign@gmail.com';               // SMTP username
+    $mail->Password   = 'K@elp1410961107';                      // SMTP password
+    $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom($email, $name);
+    $mail->addAddress('kaelpidesign@gmail.com', 'Marla');     // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = $subject;
+    $mail->Body    = $message . "<br> Sent From: Form";
+
+    $mail->send();
+    echo 'Message has been sent';
     }
-?>
+
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?> 
 
