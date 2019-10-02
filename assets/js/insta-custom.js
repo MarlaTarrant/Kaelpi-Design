27b35df782585a20ebd600f29a96777296a1a612
Marla Tarrant <tarrantmarlajean@gmail.com>
2019-10-02 19:09:43 +0200

    instafeed changes

--- assets/js/insta-custom.js
+++ assets/js/insta-custom.js
@@ -7,9 +7,7 @@ $( document ).ready(function() {
     resolution: 'standard_resolution',
     accessToken: '12557095608.1677ed0.367a8fe7331446998a81d4c0c274a691',
     sortBy: 'most-recent',
-    template: '<div class="col-lg-2 gallery instaimg"><a href="{{link}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+    template: '<div class="col-lg-2-2 gallery instaimg"><a href="{{link}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
     });
     userFeed.run ();
-
-
 });
