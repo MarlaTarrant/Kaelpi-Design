3eb1e8eada94dfba1f6ad23c885b61f584e6e07b
Marla Tarrant <tarrantmarlajean@gmail.com>
2019-10-03 13:46:33 +0200

    section changes

--- assets/js/insta-custom.js
+++ assets/js/insta-custom.js
@@ -1,13 +1,22 @@
-$( document ).ready(function() {
+$(document).ready(function () {
+
+    var limitWidth;
+    var width = $(window).width();
+
+    if (width <= 768) {
+        limitWidth = 2;
+    } else {
+        limitWidth = 12;
+    }
 
     var userFeed = new Instafeed({
-    get: 'user' ,
-    userId: '12557095608' ,
-    limit :6,
-    resolution: 'standard_resolution',
-    accessToken: '12557095608.1677ed0.367a8fe7331446998a81d4c0c274a691',
-    sortBy: 'most-recent',
-    template: '<div class="col-lg-2-2 gallery instaimg"><a href="{{link}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+        get: 'user',
+        userId: '12557095608',
+        limit: 6,
+        resolution: 'standard_resolution',
+        accessToken: '12557095608.1677ed0.367a8fe7331446998a81d4c0c274a691',
+        sortBy: 'most-recent',
+        template: '<div class="col-lg-2 col-xs-12 col-sm-6 col-md-4 gallery instaimg"><a href="{{link}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
     });
-    userFeed.run ();
+    userFeed.run();
 });
